5ce06ebc1feba7a7f704ce34d90dc53fb58aeb08
Frederik Graakjær <eaafgra@students.eaaa.dk>
2023-01-19 11:42:43 +0100

    signin

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Users from "./users";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebase-config";
+import { useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import SignIn from "./authentication/signIn";
 
 const Home: NextPage = () => {
+  const [isAuth, setIsAuth] = useState("");
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setIsAuth("isAuth");
+      // @ts-ignore disable-next-line
+      localStorage.setItem("isAuth", true);
+    } else {
+      setIsAuth("");
+      localStorage.removeItem("isAuth");
+    }
+  });
+
   return (
     <div>
       <Head>
@@ -10,9 +31,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="robots" content="noindex"></meta>
       </Head>
-
+     
       <main>
-        <Users />
+        {isAuth ? <Users /> : <SignIn/>}
       </main>
 
       <footer></footer>
